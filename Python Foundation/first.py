e17fa561ac011bddcc39c923ba1a3cd889d7a20e
codewithnitish18 <ethereal.nitish05@gmail.com>
2026-08-15 16:33:09 +0530

    h here leraning the basic of python like variables and data types and operators

---
+++ Python Foundation/first.py
@@ -0,0 +1,44 @@
+# name = "nitish"
+# age = 22
+# course = "MCA"
+# print("Name:", name)
+# print("Age:", age)
+# print("Course:", course)
+
+
+# length = 10
+# width = 5
+# area = length * width
+# print("Area Of Rectangle:", area)
+# perimeter = 2 * (length + width)
+# print("Perimeter Of Rectangle:", perimeter)
+
+# Write a program that:
+
+# Asks the user for their name
+# Asks for their age
+# Prints:
+# Hello Nitish
+# You are 22 years old
+
+# name = input("Enter your Name:")
+# age = int(input("Enter your age:"))
+# print("Hello", name)
+# print("you are ", age, "years old")
+
+#              Write a Python program that:
+
+# Takes 5 subject marks from the user.
+# Calculates the total marks.
+# Calculates the percentage.
+# Prints both.
+
+math = int(input("enter a number:"))
+science = int(input("enter a number:"))
+hindi = int(input("enter a number:"))
+english = int(input("enter a number:"))
+computer = int(input("enter a number:"))
+total_marks =  math + science + hindi + english + computer
+percentage = total_marks / 500 * 100
+print("total marks:", total_marks)
+print("percentage:", percentage)
---
+++ Python Foundation/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+name = "nitish"
+# age = 22
+# course = "MCA"
+# print("Name:", name)
+# print("Age:", age)
+# print("Course:", course)
