aae5b0d657cee3eacb5ced5f6f1e191915d550ea
codewithnitish18 <ethereal.nitish05@gmail.com>
2026-08-18 23:48:08 +0530

    finding maximum element

---
+++ Python Foundation/python loop/maximum.py
@@ -0,0 +1,32 @@
+# numbers = [7,12,4,19,8]
+# largest = numbers[0]
+# for number in numbers:
+#     if number > largest:
+#         largest = number
+# print("the largest number is:", largest)
+
+
+# numbers = [2,5,7,19,12,19]
+# largest = numbers[0]
+# second_largest = numbers[1]
+# if numbers[1] > numbers[0]:
+#     largest = numbers[1]
+#     second_largest = numbers[0]
+# else:
+#     largest = numbers[0]
+#     second_largest = numbers[1]
+
+# for number in numbers[2:]:
+#     if number > largest:
+#         second_largest = largest
+#         largest = number
+#     elif number > second_largest:
+#      second_largest = number
+# print("first and second largest numbers are:", largest, second_largest)
+
+
+
+
+
+
+numbers = [2,]
