e085cef203632a877ffb1d0701b1dec2e1148f28
codewithnitish18 <ethereal.nitish05@gmail.com>
2026-08-20 20:00:26 +0530

    finding the target value in binary search

--- Python Foundation/python loop/maximum.py
+++ Python Foundation/python loop/maximum.py
@@ -29,4 +29,43 @@
 
 
 
-numbers = [2,]
+# numbers = [2,]
+# if len(numbers) < 2:
+#     print("there is no second largest numbers in the list..")
+# else:
+#     largest = numbers[0]
+#     second_largest = numbers[1]
+
+#     if numbers[1] > numbers[0]:
+#         largest = numbers[1]
+#         second_largest = numbers[0]
+#     else:
+#         largest = numbers[0]
+#         second_largest = numbers[1]
+
+#     for number in numbers[2:]:
+#         if number > largest:
+#             second_largest = largest
+#             largest = number
+#         elif number > second_largest:
+#             second_largest = number
+#             print("laregst number is:", largest)
+#             print("second largest number is:", second_largest)
+
+
+arr = [1,2,4,6,9]
+target = 4
+low = 0
+high = len(arr) - 1 
+
+while(low <= high):
+    mid = (low + high)//2
+    if arr[mid] == target:
+        print(f'target found at the index {mid}')
+        break
+    elif arr[mid] < target:
+        low = mid + 1
+    elif arr[mid] > target:
+        high = mid - 1
+else:
+  print("Number not found")
